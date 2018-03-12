0871f53c09ba5db37ec5bcca6caaac690836db63
Yongkyu (laptop-linux) <jyg1124@gmail.com>
2018-03-11 22:19:57 -0500

    add model: single-commodity flow-based model (paper model and revised model)

--- TestSets/mptsps/JULIA/mptsps_flow_revised.jl
+++ TestSets/mptsps/JULIA/mptsps_flow_revised.jl
@@ -1,42 +1,30 @@
 # Source
 #  Perboli et al., "A Progressive Hedging Method for the Multi-path Travelling Salesman Problem with Stochastic Travel Times," vol.28, pp. 65-86, 2017
-#
+
 # Decision variables
+#  (1st stage, real)    ϕ[i,j]: the flow on the arc (i,j) ∈ N × N
 #  (1st stage, binary)  y[i,j] = 1 if node j ∈ N is visited just after node i ∈ N, 0 otherwise.
 #  (2nd stage, binary)  x[s,i,j,p] = 1 if path p ∈ K between nodes (i,j) ∈ N × N  is selected at the second stage under scenario s ∈ S, 0 otherwise.
-#
-# Note
-#  Two lines (54, 108) for the 'subtour elimination' is commented
-#  since it causes out of memory when finding power set of set of nodes .
 
 using JuMP, StructJuMP
 
 type MPTSPsModel
     # Sets
     N   # set of nodes of the graph : i,j ∈ N
-    U   # set of subsets of nodes in N : u ∈ U
-    K   # set of paths between the pair of nodes : p ∈ K
+    K   # set of paths between the pair of nodes : k ∈ K
     S   # set of time scenarios : s ∈ S
 
     # Parameters
-    Cs  # nonnegative unit random travel time cost under the time scenario s ∈ S : Cs[s][i][j][p]
+    Cs  # nonnegative unit random travel time cost under the time scenario s ∈ S : Cs[s][i][j][k]
     Ce  # nonnegative estimation of the mean unit travel time cost : Ce[i][j]
-    Δ   # the error on the travel time cost estimated for the path k ∈ K under time scenario s ∈ S : Δ[s][i][j][p] ≡ Cs[s][i][j][p] - Ce[i][j]
+    Δ   # the error on the travel time cost estimated for the path k ∈ K under time scenario s ∈ S : Δ[i][j][k][s] ≡ Cs[i][j][k][s] - Ce[i][j]
     π   # probability distribution of scenario s ∈ S : π[s] ≡ 1/nScenario
 
     MPTSPsModel() = new()
 end
 
-function mptspsdata(nScenarios::Integer, Graph::String)::MPTSPsModel
 
-    # Function for subtour elimination
-    function powerset{Int}(S::Vector{Int})
-        set = Vector{Int}[[]]
-        for s in S, i in eachindex(set)
-            push!(set, [set[i] ; s])
-        end
-        return set
-    end
+function mptspsdata(nScenarios::Integer, Graph::String)::MPTSPsModel
 
     # Set paths
     cd(dirname(Base.source_path()))
@@ -51,7 +39,6 @@ function mptspsdata(nScenarios::Integer, Graph::String)::MPTSPsModel
     tsp.N = 1:readdlm("$GRAPH_DIR/prob.txt")[4,2]
     tsp.K = 1:readdlm("$GRAPH_DIR/prob.txt")[5,2]
     tsp.S = 1:nScenarios
-    #tsp.U = powerset(convert(Array{Int},tsp.N)) # for subtour elimination
 
     tsp.Cs = Array{Array{Array{Float64}}}[]
     for s in tsp.S
@@ -63,7 +50,7 @@ function mptspsdata(nScenarios::Integer, Graph::String)::MPTSPsModel
             arr_1 = Array{Float64}[]
             for j in tsp.N
                 arr_2 = Float64[]
-                for p in tsp.K
+                for k in tsp.K
                     push!(arr_2, scenario_array[v])
                     v += 1
                 end
@@ -84,8 +71,8 @@ function mptspsdata(nScenarios::Integer, Graph::String)::MPTSPsModel
 
     tsp.Δ = deepcopy(tsp.Cs)
     for s in tsp.S
-        for i in tsp.N, j in tsp.N, p in tsp.K
-            tsp.Δ[s][i][j][p] -= tsp.Ce[i][j]
+        for i in tsp.N, j in tsp.N, k in tsp.K
+            tsp.Δ[s][i][j][k] -= tsp.Ce[i][j]
         end
     end
 
@@ -94,37 +81,71 @@ function mptspsdata(nScenarios::Integer, Graph::String)::MPTSPsModel
     return tsp
 end
 
-function mptsps_subtour(nScenarios::Integer, Graph::String)::JuMP.Model
+function mptsps_flow(nScenarios::Integer, Graph::String)::JuMP.Model
     # get model data
     tsp = mptspsdata(nScenarios, Graph)
 
-    # construct JuMP.Model
+    #construct JuMP.Model
     model = StructuredModel(num_scenarios = nScenarios)
 
     ## add 1st stage components
-    @variable(model, y[i = tsp.N, j = tsp.N], Bin)
+    @variables model begin
+        ϕ[i = tsp.N, j = tsp.N] >= 0, Cont
+        y[i = tsp.N, j = tsp.N], Bin
+    end
     @objective(model, Min, sum(tsp.Ce[i][j]*y[i,j] for i in tsp.N for j in tsp.N))
     @constraints model begin
         [i = tsp.N], sum(y[i,j] for j in tsp.N if j ∉ i) == 1
         [j = tsp.N], sum(y[i,j] for i in tsp.N if i ∉ j) == 1
-        #[u=tsp.U[2:end-1]], sum(y[i,j] for i in u for j in tsp.N if j ∉ u) >= 1 # subtour elimination constraints
+        [i = tsp.N ; i ∉ 1], sum(ϕ[i,j] for j in tsp.N) - sum(ϕ[j,i] for j in tsp.N if j ∉ 1) == 1
+        [i = tsp.N, j = tsp.N ; i ∉ 1], ϕ[i,j] <= (length(tsp.N)-1)*y[i,j]
     end
 
     ## add 2nd stage components
     for s in tsp.S
         sb = StructuredModel(parent = model, id = s, prob = tsp.π[s])
-        @variable(sb, x[i = tsp.N, j = tsp.N, p = tsp.K], Bin)
-        @objective(sb, Min, sum(tsp.Δ[s][i][j][p]*x[i,j,p] for i in tsp.N for j in tsp.N for p in tsp.K))
-        @constraint(sb, [i = tsp.N, j = tsp.N], sum(x[i,j,p] for p in tsp.K) == y[i,j])
+        @variable(sb, x[s = tsp.S, i = tsp.N, j = tsp.N, p = tsp.K], Bin)
+        @objective(sb, Min, sum(tsp.Δ[s][i][j][p]*x[s,i,j,p] for i in tsp.N for j in tsp.N for p in tsp.K))
+        @constraint(sb, [i = tsp.N, j = tsp.N], sum(x[s,i,j,p] for p in tsp.K) == y[i,j])
     end
 
     return model
 end
 
-# test ( nScenario ∈ [1,...,100] and Graph ∈ {"D0_50", "D1_50", "D1_100", "D2_50", "D3_50"} )
-model = mptsps_subtour(3, "D0_50")
+model = mptsps_flow(10, "D0_50")
 
+# test ( nScenario ∈ [1,...,100] and Graph ∈ {"D0_50", "D1_50", "D1_100", "D2_50", "D3_50"} )
 using CPLEX
+nScenarios, Graph = 10, "D0_50"
+# construct model =====================#
+tsp = mptspsdata(nScenarios, Graph)
+model = StructuredModel(num_scenarios = nScenarios)
+@variables model begin
+    ϕ[i = tsp.N, j = tsp.N] >= 0, Cont
+    y[i = tsp.N, j = tsp.N], Bin
+end
+@objective(model, Min, sum(tsp.Ce[i][j]*y[i,j] for i in tsp.N for j in tsp.N))
+@constraints model begin
+    [i = tsp.N], sum(y[i,j] for j in tsp.N if j ∉ i) == 1
+    [j = tsp.N], sum(y[i,j] for i in tsp.N if i ∉ j) == 1
+    [i = tsp.N ; i ∉ 1], sum(ϕ[i,j] for j in tsp.N) - sum(ϕ[j,i] for j in tsp.N if j ∉ 1) == 1
+    [i = tsp.N, j = tsp.N ; i ∉ 1], ϕ[i,j] <= (length(tsp.N)-1)*y[i,j]
+end
+for s in tsp.S
+    sb = StructuredModel(parent = model, id = s, prob = tsp.π[s])
+    @variable(sb, x[i = tsp.N, j = tsp.N, p = tsp.K], Bin)
+    @objective(sb, Min, sum(tsp.Δ[s][i][j][p]*x[i,j,p] for i in tsp.N for j in tsp.N for p in tsp.K))
+    @constraint(sb, [i = tsp.N, j = tsp.N], sum(x[i,j,p] for p in tsp.K) == y[i,j])
+end
+# solve & print result =======================#
 setsolver(model,CplexSolver(CPXPARAM_TimeLimit = 30))
 solve(model)
 println("Obj = $(getobjectivevalue(model))")
+println("Solutions:")
+for i in tsp.N
+    for j in tsp.N
+        if i != j && getvalue(y[i,j]) == 1
+            println("y[$i,$j] = $(getvalue(y[i,j]))")
+        end
+    end
+end
