9192687e22ea6ad468efa7bc8f4a2024b1ee3927
Yongkyu Cho <choy@compute001.mcs.anl.gov>
2018-04-24 20:37:43 -0500

    sucw sparsity figure
    Former-commit-id: f8e8e9074b8b40bea16e7e51d1cdf9c5c2f37962

--- TestSets/SUCW/JULIA/sucw_main.jl
+++ TestSets/SUCW/JULIA/sucw_main.jl
@@ -9,7 +9,7 @@ using SmpsWriter, SparsityAnalyzer
 
 ## set parameters for instance
 SD = "FallWD"       # Season-Day
-nS = 3             # number of scenarios
+nS = 1             # number of scenarios
 
 ## set file name and path
 INSTANCE = "SUCW_$(SD)_$(nS)"
