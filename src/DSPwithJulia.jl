fc946baefd5ab8069aab1c009910225488ec10f1
Yongkyu (laptop-linux) <jyg1124@gmail.com>
2018-05-03 23:52:43 -0500

    add: instance size analyzer

--- src/DSPwithJulia.jl
+++ src/DSPwithJulia.jl
@@ -9,6 +9,13 @@ INSTANCE_NAME = string(PROB_NAME,PARAMETER)
 cd(dirname(Base.source_path()))
 readSmps("../TestSets/$PROB_NAME/SMPS/$SMPS_FILE_NAME")
 
+using Dsp
+cd(dirname(Base.source_path()))
+SMPS_PATH = "../experiment/SMPS/DCAP/DCAP_2_3_3_500"
+parameter_file = "../experiment/DSP/parameters.txt"
+readSmps(SMPS_PATH)
+optimize(solve_type = :Dual, param = parameter_file)
+
 solve_types = [:Extensive, :Dual, :Benders]
 #alg = :Dual # one of [:Dual, :Benders, :Extensive]
 #optimize(solve_type = solve_types[1], param = "myparam.txt")
