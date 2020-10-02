bc367a5ac302646fc37d1033d230c6117c9d0376
Kibaek Kim <kibaek.k@gmail.com>
2020-10-02 16:30:12 -0500

    Update dro.jl

--- src/dro.jl
+++ src/dro.jl
@@ -7,27 +7,27 @@ NOTE: This file can be read by DSP.
 The format is as follows:
 
 line 1: wasserstein ball size
-line 2: number of discretized points of support
+line 2: number of references
 line 3-: wasserstein distance
 """
 function write_wasserstein_dro(
     num_discretizations::Int,
     num_references::Int,
     reference_probability::Vector{Float64},
-    wasserstein_distance::Array{Float64,2}, # Dimension: (num_discretizations) times (num_discretizations + num_references)
+    wasserstein_distance::Array{Float64,2}, # Dimension: (num_references) times (num_discretizations + num_references)
     ϵ::Float64,
     filename::String
 )
     m, n = size(wasserstein_distance)
-    if m == num_discretizations && n == num_discretizations + num_references
+    if m == num_references && n == num_discretizations + num_references
         @error "The dismension of wasserstein_distance does not match."
         return
     end
 
     fp = open("$filename.dro","w")
     println(fp, ϵ)
-    println(fp, num_discretizations)
-    for s = 1:num_discretizations
+    println(fp, num_references)
+    for s = 1:num_references
         if s > 1
             print(fp, ",")
         end
@@ -35,7 +35,7 @@ function write_wasserstein_dro(
     end
     print(fp, "\n")
     for i = 1:(num_discretizations+num_references)
-        for s = 1:num_discretizations
+        for s = 1:num_references
             if s > 1
                 print(fp, ",")
             end
